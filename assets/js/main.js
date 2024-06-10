76c016771f47f05d9223692356712541a210f55a
karu5 <fabiolakmschmidt@gmail.com>
2024-06-10 12:01:48 -0400

    1er trabajo mod3

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const precioInicial = 400000;
+let cantidad = 0;
+let valorTotal = 0;
+
+
+document.querySelector(".precioInicial").innerHTML = precioInicial;
+document.querySelector(".cantidad").innerHTML = cantidad;
+document.querySelector(".valorTotal").innerHTML = valorTotal;
